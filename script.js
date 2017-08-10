3170e965bd3fce5fa35bbd3d7aa1e49711267740
Taylor Haase <taylor_haase@hotmail.com>
2017-08-09 20:44:54 -0700

    ready to submit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 var penColor="black";
 function setPenColor(pen){
     penColor = pen;
+    let currentColor = document.getElementsByClassName("current")[0];
+
+    currentColor.style.backgroundColor = pen;
 };
 
 function setPixelColor(pixel){
     pixel.style.backgroundColor= penColor;
 };
-
-var currentColor="black";
-function setCurrentColor() {
-    let currentColor = this.id;
-    penColor.style.backgroundColor = penColor;
-}
